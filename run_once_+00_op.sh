8661c72c63d4fd97de4c37d6216b7d092bfa6a53
Zsolt Szalacsi <zsolt@szalacsi.com>
2021-11-16 11:11:49 +0000

    setup one password

---
+++ run_once_+00_op.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# 1Password command line tool installation
+
+# Create .local/bin if doesnt exist
+if ! [ -d ~/.local/bin ]
+then
+    mkdir -p ~/.local/bin
+fi
+
+# Add local bin to path
+if ! [[ $PATH =~ "$HOME/.local/bin:" ]]
+then
+    export PATH=$HOME/.local/bin:$PATH
+fi
+
+curl -s https://cache.agilebits.com/dist/1P/op/pkg/v1.12.3/op_linux_amd64_v1.12.3.zip --output /tmp/op.zip
+cd /tmp
+unzip op.zip
+gpg --receive-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
+gpg --verify op.sig op
+mv op ~/.local/bin
+op signin my zsolt@szalacsi.com
