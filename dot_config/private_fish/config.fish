3783058889b531735bef08d081c5ed80ba9a74a7
Zsolt Szalacsi <zsolt@szalacsi.com>
2021-11-23 18:22:45 +0000

    vim

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,8 +4,10 @@ set fish_greeting ""
 fish_add_path ~/.local/bin
 fish_add_path ~/bin
 
-set EDITOR vi
-set KUBE_EDITOR vi
+set EDITOR vim
+set KUBE_EDITOR vim
+
+alias vi vim
 
 # Generated for envman. Do not edit.
 test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
