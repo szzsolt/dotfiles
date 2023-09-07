103f35234538e4db30b773eda667a720a9e6593a
Zsolt Szalacsi <zsolt@szalacsi.com>
2023-09-07 10:50:30 +0100

    syncing with desktop rhel8

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,7 @@ set fish_greeting ""
 
 fish_add_path ~/.local/bin
 fish_add_path ~/bin
+fish_add_path ~/go/bin
 
 set EDITOR vim
 set KUBE_EDITOR vim
@@ -12,3 +13,5 @@ alias vi vim
 # Generated for envman. Do not edit.
 test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
 
+
+sh ~/.local/bin/agent-bridge.sh
