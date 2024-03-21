1174c39511d3f5a6eb99b2fff3ed873571db3a44
Zsolt Szalacsi <zsolt.szalacsi@stepforward.io>
2024-03-21 10:33:43 +0000

    nvim and prompt changes

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,13 +3,10 @@ return {
   {
     "lalitmee/cobalt2.nvim",
     event = { "ColorSchemePre" }, -- if you want to lazy load
-    dependencies = { "tjdevries/colorbuddy.nvim" },
+    dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
     init = function()
       require("colorbuddy").colorscheme("cobalt2")
     end,
-  },
-  {
-    "arcticicestudio/nord-vim",
   },
 
   -- Configure LazyVim to load color scheme
